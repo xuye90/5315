493262e5f7ae5b4be240c2ac5c61f1ecdfe75fd7
ye <xuy12@seattleu.edu>
2018-04-29 11:46:02 -0700

    hw2 prep files

--- hw2data.js
+++ hw2data.js
@@ -1,12 +1,124 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "email": "Accountant"
+      "name": "Spencer Bond",
+      "email": "spencerbond@gmail.com",
+      "street address": "70 Bowman Street",
+      "city": "Bellevue",
+      "state": "Washington",
+      "phone": "4252718568"
     },
     {
-      "name": "Brielle Williamson",
-      "email": "4804"
+      "name": "Gordon Slater",
+      "email": "gordon.slater@gmail.com",
+      "street address": "123 6th Street",
+      "city": "Renton",
+      "state": "Washington",
+      "phone": "4252328790"
+    }, 
+    {
+      "name": "Angela Miller",
+      "email": "angela.miller@hotmail.com",
+      "street address": "71 Pilgrim Avenue",
+      "city": "Issaquah",
+      "state": "Washington",
+      "phone": "7342459764"
+    },
+    {
+      "name": "Wanda Nash",
+      "email": "wanda.nash@gmail.com",
+      "street address": "744 Shirley Avenue",
+      "city": "Bellevue",
+      "state": "Washington",
+      "phone": "4578462846"
+    },
+    {
+      "name": "Grace Paterson",
+      "email": "grace.paterson@gmail.com",
+      "street address": "4 Goldfield Road",
+      "city": "Kirkland",
+      "state": "Washington",
+      "phone": "4295736485"
+    },
+    {
+      "name": "Owen Young",
+      "email": "sowen.young@gmail.com",
+      "street address": "514 S. Magnolia Street",
+      "city": "Bellevue",
+      "state": "Washington",
+      "phone": "4738475937"
+    },
+    {
+      "name": "Katherine Smith",
+      "email": "katherine.smith@gmail.com",
+      "street address": "108 Cypress Lane",
+      "city": "Bothell",
+      "state": "Washington",
+      "phone": "4857362849"
+    },
+    {
+      "name": "Jonathan Ellison",
+      "email": "jonathan.ellison@gmail.com",
+      "street address": "193 Indian Summer Avenue",
+      "city": "Sammamish",
+      "state": "Washington",
+      "phone": "4596732738"
+    },
+    {
+      "name": "Lauren Turner",
+      "email": "lauren.turner@gmail.com",
+      "street address": "139 Hartford Dr",
+      "city": "Bellevue",
+      "state": "Washington",
+      "phone": "7364859265"
+    },
+    {
+      "name": "Andrea Terry",
+      "email": "andrea.terry@hotmail.com",
+      "street address": "8562 Pearl Street",
+      "city": "Bellevue",
+      "state": "Washington",
+      "phone": "5483967233"
+    },
+    {
+      "name": "Alexander Graham",
+      "email": "alexander.graham@gmail.com",
+      "street address": "238 Bowman Drive",
+      "city": "Renton",
+      "state": "Washington",
+      "phone": "3758395768"
+    },
+    {
+      "name": "Isaac Morgan",
+      "email": "isaac.morgan@gmail.com",
+      "street address": "7835 Ohio Street",
+      "city": "Redmond",
+      "state": "Washington",
+      "phone": "6374928465"
+    },
+    {
+      "name": "Sonia Grant",
+      "email": "sonia.grant@gmail.com",
+      "street address": "291 Magnolia Avenue",
+      "city": "Bellevue",
+      "state": "Washington",
+      "phone": "7635485937"
+    },
+    {
+      "name": "Michael Avery",
+      "email": "michael.avery@gmail.com",
+      "street address": "33 Studebaker Street",
+      "city": "Redmond",
+      "state": "Washington",
+      "phone": "6748374657"
+    },
+    {
+      "name": "Thomas Bower",
+      "email": "thomas.bower@gmail.com",
+      "street address": "456 Vine Drive",
+      "city": "Everret",
+      "state": "Washington",
+      "phone": "8475738587"
     }
   ]
 }
